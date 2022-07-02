ee43cb31f070bca858e5f66bea946f4311ae9055
mgamboa <gamboa.martin@gmail.com>
2022-07-02 10:57:58 -0500

    v0.19.0 Se ajusta html en directivas

--- src/directivas.php
+++ src/directivas.php
@@ -4,7 +4,7 @@ use gamboamartin\errores\errores;
 
 class directivas{
     protected errores $error;
-    private html $html;
+    protected html $html;
     public function __construct(html $html){
         $this->error = new errores();
         $this->html = $html;

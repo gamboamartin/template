7408d614b974b3cbdeb4f2eaa6ec2e4dec368125
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-21 13:02:57 -0500

    v0.48.1 Se genera ut required text

--- tests/src/directivasTest.php
+++ tests/src/directivasTest.php
@@ -139,6 +139,29 @@ class directivasTest extends test {
     }
 
     /**
+     */
+    #[NoReturn] public function test_input_text_required(): void
+    {
+        errores::$error = false;
+        $html_ = new html();
+        $html = new directivas($html_);
+        //$html = new liberator($html);
+        $_GET['session_id'] = 1;
+
+        $row_upd = new stdClass();
+        $disable = false;
+        $value_vacio = true;
+        $name = 'z';
+        $place_holder = 'd';
+
+        $resultado = $html->input_text_required($disable, $name, $place_holder, $row_upd, $value_vacio);
+        $this->assertIsString($resultado);
+        $this->assertNotTrue(errores::$error);
+        $this->assertEquals("<div |class|><input type='text' name='z' value='' |class|  required id='z' placeholder='d' /></div>", $resultado);
+        errores::$error = false;
+    }
+
+    /**
      * @throws JsonException
      */
     #[NoReturn] public function test_label_input(): void

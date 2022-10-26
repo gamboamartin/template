aa4deb07f9ddbf80f3bc4e274a1ea72c364d3fd4
mgamboa <gamboa.martin@gmail.com>
2022-10-26 11:52:31 -0500

    v0.107.4 Se integra input password

--- src/directivas.php
+++ src/directivas.php
@@ -400,6 +400,41 @@ class directivas{
         return $div;
     }
 
+    /**
+     * Genera un input tipo required
+     * @param stdClass $row_upd Registro obtenido para actualizar
+     * @param bool $disable si disabled retorna el input como disabled
+     * @param string $name Usado para identificador css name input y place holder
+     * @param string $place_holder Texto a mostrar en el input
+     * @param bool $value_vacio Para altas en caso de que sea vacio o no existe el key
+     * @return array|string
+     */
+    public function input_password(bool $disable, string $name, string $place_holder, stdClass $row_upd,
+                                        bool $value_vacio ): array|string
+    {
+
+        $valida = $this->valida_data_label(name: $name,place_holder:  $place_holder);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar datos ', data: $valida);
+        }
+
+        $init = $this->init_text(name: $name,place_holder:  $place_holder, row_upd: $row_upd,value_vacio:  $value_vacio);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al inicializar datos', data: $init);
+        }
+
+        $html= $this->html->password(disabled:$disable, id_css: $name, name: $name, place_holder: $place_holder,
+            required: true, value: $init->row_upd->$name);
+
+        $div = $this->html->div_label(html:  $html,label:$init->label);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
+        }
+
+        return $div;
+
+    }
+
 
     /**
      * Funcion de inicializacion de datos para inputs
